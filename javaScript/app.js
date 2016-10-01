81aea05506fbb3220253204558d639354269c186
Scott Diallo <diallot@me.com>
2016-10-01 14:49:36 -0700

    method of undefined resolve

--- javaScript/app.js
+++ javaScript/app.js
@@ -12,58 +12,58 @@ $(document).ready(function () {
     //
     //    //populate questionnaires
     var questions = [
-    //question_1
+        //question_1
         {
             question: "What's the longest river in the U.S ?",
             choices: ['Mississipi River', 'Missouri River', 'Rio Grande', 'Yukon River'],
             answer: 0,
         },
-    // question_2
+        // question_2
         {
             question: 'Who among these famous people made the most impact in the world ?',
             choices: ['Steve Jobs', 'Nicholas Tesla', 'Scott Maserati', 'Elvis Paisley'],
             answer: 0,
-    },
-    //question_3
+        },
+        //question_3
         {
             question: 'Which U.S President promise the Americans public that we will go to the moon?',
             choices: ['Franklin D Roosevelt', 'Abraham Lincoln', 'Jimmy Carter', 'John F Kennedy'],
             answer: 3,
-    },
-    //question_4
+        },
+        //question_4
         {
             question: 'There was like a Uber like in the 1914, which was it?',
             choices: ['Jitney Bus', 'ouwBer', 'takeMethere', 'TaxiNow'],
             answer: 0,
         },
-    //question_5
+        //question_5
         {
             question: 'Is it true that about 100 acres of pizza are served in the U.S everyday?',
             choices: ['True', 'False'],
             answer: 0,
             detail: 'lorem ipsum'
         },
-    //question_6
+        //question_6
         {
             question: 'Which is the leading cause of death in the U.S',
             choices: ['Malaria', 'Cigarette', 'Car Accident', 'Heart Disease'],
             answer: 3,
             detail: 'lorem ipsum'
         },
-    //question_7
+        //question_7
         {
             question: 'Louisiana purchased from France in 18xx, which state was sold to U.S in 1867 from who?',
             choices: ['Alaska from Russia', 'New York from England', 'Maine from Canada', 'Texas from Mexico'],
             answer: 0,
         },
-    //question_8
+        //question_8
         {
             question: 'Which American company has more operating cash the the us treasury?',
             choices: ['Google', 'Tesla', 'Apple', 'Microsoft'],
             answer: 2,
-    },
+        },
 
-    //question_9
+        //question_9
         {
             question: 'What is the most popular fruit in the US?',
             choices: ['Apple', 'Orange', 'Guava', 'Banana'],
@@ -71,13 +71,13 @@ $(document).ready(function () {
 
         },
 
-    //question_10
+        //question_10
         {
             question: 'Who signed the declration of independence paper?',
             choices: ['John Hancock', 'James Madison', 'George Washington', 'Jimmy Carter'],
             answer: 0,
         },
-];
+    ];
     // variables
     var questionNum = 0;
     var questionTotal = questions.length;
@@ -145,4 +145,3 @@ $(document).ready(function () {
         correctAnswerTotal = 0;
     });
 });
-//testing
